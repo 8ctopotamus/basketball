b7dabb609e2dbb590d4933e1915de694ad540561
8ctopotamus <joshnaylor88@gmail.com>
2017-09-19 21:55:32 -0500

    Scoring, throwing, particle effects

--- Assets/Project/Scripts/Player.cs
+++ Assets/Project/Scripts/Player.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
+	public Ball ball;
+	public GameObject playerCamera;
+
+	public float ballDistance = 2f;
+	public float ballThrowingForce = 5f;
+
+	public bool holdingBall = true;
+
 	void Start () {
-		
+		ball.GetComponent<Rigidbody> ().useGravity = false;
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		
+		if (holdingBall) {
+			ball.transform.position = playerCamera.transform.position + playerCamera.transform.forward * ballDistance;
+
+			if (Input.GetMouseButtonDown(0)) {
+				holdingBall = false;
+				ball.ActivateTrail ();
+				ball.GetComponent<Rigidbody> ().useGravity = true;
+				ball.GetComponent<Rigidbody> ().AddForce (playerCamera.transform.forward * ballThrowingForce);
+			}
+		}
 	}
 }
---
+++ Assets/Project/Scripts/ScoreArea.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ScoreArea : MonoBehaviour {
+
+	public GameObject effectObject;
+
+	void Start () {
+		effectObject.SetActive (false);
+	}
+
+	void OnTriggerEnter (Collider otherCollider) {
+		if (otherCollider.GetComponent<Ball> () != null) {
+			effectObject.SetActive (true);
+		}
+	}
+
+}
